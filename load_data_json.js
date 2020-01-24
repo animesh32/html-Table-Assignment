3c67a0a2b2f56ffd8f9995f2a4ccd76efa022e02
animesh <animesh.garg@sourcefuse.com>
2020-01-24 17:35:03 +0530

    bugs fixed of previous

--- load_data_json.js
+++ load_data_json.js
@@ -1,5 +1,7 @@
+// stores the headers of table
+let tableHeaders=['FIRST NAME','MIDDLE NAME','LAST NAME','EMAIL','PhONE NO','ROLE','ADDRESS',"FUNCTIONS"," "];
+
 // this is json file that contains data of users
-let columns=['FIRST NAME','MIDDLE NAME','LAST NAME','EMAIL','PHONE NO','ROLE','ADDRESS',"FUNCTIONS"," "];
 let usersData=[     
     {
         firstName: "Aniemsh",
@@ -11,7 +13,7 @@ let usersData=[
         address: "Mohali" 
     },
     {
-        firstName: "Harsh",
+        firstName: "harsh",
         middleName: "deep",
         lastName: "Dhiman",
         email: "harsh.com",
@@ -32,20 +34,21 @@ let usersData=[
     }
 ];
 
+//EVENT LISTENER THAT GETS TRIGGERED ON LOAD DATA BUTTON
+document.getElementById("loadbutton").addEventListener("click",loadDataEvent)
 
-document.getElementById("loadbutton").addEventListener("click",event)
+//EVENT THAT OCCURS ON CLICKING LOAD DATA BUTTON
+function loadDataEvent(){  
+    //ALL  ELEMENTS IN BODY ARE REMOVED SO TO DISPLAY NEXT PAGE
+    body.innerHTML=""
 
-
-function event(){                            
-
-    //ALL DIV ELEMENT IN BODY ARE REMOVED SO TO DISPLAY NEXT PAGE
-    document.body.innerHTML=""
-    let div=document.createElement("div")
-    div.align="center"
-    div.style.marginTop= "10px"
-    div.style.marginBottom= "10px"
-    div.innerHTML='<button id="refresh" onclick="refreshFun()">Refresh Data</button>';
-    body.appendChild(div)
+    //CREATING REFRESH BUTTON ON TOP
+    let divElement=document.createElement("div")
+    divElement.align="center"
+    divElement.style.marginTop= "10px"
+    divElement.style.marginBottom= "10px"
+    divElement.innerHTML='<button id="refresh" onclick="refreshFun()">Refresh Data</button>';
+    body.appendChild(divElement)
     
     //CREATING NEW TABLE ELEMENT
     createTable();
@@ -53,34 +56,30 @@ function event(){
     //FILL TABLE WITH USERS DATA
     fillTable();
 
-    //Creating ADD BUTTON
-    div=document.createElement("div")
-    div.align="center"
-    div.style.marginTop= "10px"
-    div.style.marginBottom= "10px"
-    div.innerHTML='<button id="add" onclick="addData()">Add Data</button>';
-    body.appendChild(div)
+    //Creating ADD BUTTONid="add"id="add"="add"id="add"d="add"
+    divElement=document.createElement("div")
+    divElement.align="center"
+    divElement.style.marginTop= "10px"
+    divElement.style.marginBottom= "10px"
+    divElement.innerHTML='<button id="add" onclick="addData()">Add Data</button>';
+    body.appendChild(divElement)
 }
 
-function addData(){
-
-}
-
-
+//CREATES TABLE ELEMENT WITH HEADERS FROM tableHeaders ARRAY
 function createTable(){
-    let div=document.createElement("div")
-    div.id="div"
-    div.className="w3-container"
-    document.getElementsByTagName("body")[0].appendChild(div);
+    let divElement=document.createElement("div")
+    divElement.id="table-div"
+    divElement.className="w3-container"
+    document.getElementsByTagName("body")[0].appendChild(divElement);
     let myTable=document.createElement("table");
     myTable.id="mytable"
     myTable.className="w3-table-all w3-centered"
-    document.getElementById("div").appendChild(myTable);
-    let tr=myTable.insertRow();
-    for(let i=0;i<columns.length;i++){
-        let th=document.createElement("th");
-        tr.appendChild(th);
-        th.appendChild(document.createTextNode(columns[i]));
+    document.getElementById("table-div").appendChild(myTable);
+    let tableRow=myTable.insertRow();
+    for(let i=0;i<tableHeaders.length;i++){
+        let tableCell=document.createElement("th");
+        tableRow.appendChild(tableCell);
+        tableCell.appendChild(document.createTextNode(tableHeaders[i]));
     }
 }
 
@@ -88,106 +87,106 @@ function createTable(){
 function fillTable(){
     let mytable=document.getElementsByTagName("table")[0];
 
+    //INSERTING USER DATA IN TABLE
     for(let i=0;i<usersData.length;i++){
-        let tr=mytable.insertRow()
-        tr.id="tr"+i;
+        let tableRow=mytable.insertRow()
+        tableRow.id="tr"+i;
         for(let j in usersData[i]){
-            let td=tr.insertCell()
-            td.appendChild(document.createTextNode(usersData[i][j]));
+            let tableCell=tableRow.insertCell()
+            tableCell.appendChild(document.createTextNode(usersData[i][j]));
         }
-        let td=tr.insertCell()
-        let button1=document.createElement("button");
-        button1.className="w3-button w3-cyan";
-        button1.appendChild(document.createTextNode("Edit"))
-        td.appendChild(button1);
-        button1.onclick=edit
-        // let button2=document.createElement("button");
-        // button2.className="w3-button w3-teal";
-        // button2.appendChild(document.createTextNode("Delete"))
-        // button2.onclick="del()"
-        // td.appendChild(button1);
-        td.appendChild(button1);
-        td= tr.insertCell();
-        td.innerHTML='<button class="w3-button w3-teal" onclick="del(this)">Delete</button>'
+        // INSERTING EDIT AND DELETE BUTTTONS
+        let tableCell=tableRow.insertCell()
+        tableCell.innerHTML='<button class="w3-button w3-teal">Edit</button>'
+        tableCell.firstChild.onclick=edit
+        tableCell= tableRow.insertCell();
+        tableCell.innerHTML='<button class="w3-button w3-teal" onclick="del(this)">Delete</button>'
         
     }
     
 }
-
+// TO EDIT A GIVEN ROW
 function edit(){
-    let row=this.parentNode.parentNode
-    let rowData=[]
-    let currcolumn=row.firstChild
-    let traverse=7;
-    while(traverse--){
-        rowData.push(currcolumn.textContent)
-        currcolumn=currcolumn.nextSibling
+    let currentTableRow=this.parentNode.parentNode
+   
+    let rowData=[] //TO STORE CURRENT ROW DATA IN CASE OF CANCEL
+    let currentColumn=currentTableRow.firstChild
+    let noOfCells=7;
+    while(noOfCells--){
+        rowData.push(currentColumn.textContent)
+        currentColumn=currentColumn.nextSibling
     }
-    // console.log(rowData)
-    traverse=7
-    currcolumn=row.firstChild
-    while(traverse--){
-        currcolumn.contentEditable=true
-        currcolumn=currcolumn.nextSibling
+
+    noOfCells=7
+    currentColumn=currentTableRow.firstChild
+    while(noOfCells--){
+        currentColumn.contentEditable=true
+        currentColumn=currentColumn.nextSibling
     }
-    let parent=this.parentNode.nextSibling
+
+    //CHANGING EDIT AND DELETE BUTTONS TO SAVE AND CANCEL
+    let delButtonCell=this.parentNode.nextSibling
     this.textContent="Save"
-    this.onclick=function(){
-        // console.log(this)
-        traverse=7
-        currcolumn=row.firstChild
-        while(traverse--){
-        currcolumn.contentEditable=false
-        currcolumn=currcolumn.nextSibling
+    this.onclick=function(){//ACTION ON CLICKING SAVE BUTTON
+        noOfCells=7
+        currentColumn=currentTableRow.firstChild
+        while(noOfCells--){
+        currentColumn.contentEditable=false
+        currentColumn=currentColumn.nextSibling
         }
+        //CANGING BUTTONS BACK TO EDIT AND DELETE
         this.textContent="Edit"
         this.onclick=edit
+        delButtonCell.innerHTML='<button class="w3-button w3-teal" onclick="del(this)">Delete</button>'
         
-        parent.innerHTML='<button class="w3-button w3-teal" onclick="del(this)">Delete</button>'
-        return;
     }
-    let secondButton=parent.firstChild
-    secondButton.textContent="Cancel"
-    secondButton.onclick=function(){
-        currcolumn=row.firstChild
-        for(traverse=0;traverse<7;traverse++){
-        currcolumn.textContent=rowData[traverse]
-        currcolumn=currcolumn.nextSibling
+    let editButtonObj=this;
+    delButtonCell.firstChild.textContent="Cancel"
+    delButtonCell.firstChild.onclick=function(){
+        noOfCells=7
+        currentColumn=currentTableRow.firstChild
+        while(noOfCells--){
+        currentColumn.contentEditable=false
+        currentColumn=currentColumn.nextSibling
         }
-        this.textContent="Edit"
-        this.onclick=edit
-        parent.innerHTML='<button class="w3-button w3-teal" onclick="del(this)">Delete</button>'
-        return;
+        editButtonObj.textContent="Edit"
+        editButtonObj.onclick=edit
+        currentColumn=currentTableRow.firstChild
+        for(let i=0;i<7;i++){
+            currentColumn.textContent=rowData[i];
+            currentColumn=currentColumn.nextSibling
 
+        }
+        this.parentNode.innerHTML='<button class="w3-button w3-teal" onclick="del(this)">Delete</button>'
     }
-
-
-}
-
-
-//event listener that gets triggered on clickcing on load data button on front page
-
-function editRow(obj){
-    console.log("we are here")
-    let parent=obj.parentNode
-    parent.innerHTML='<<button class="w3-button w3-teal" onclick="save(this)">Save</button>'
-    parent=parent.parentNode
     
-    parent.lastChild.innerHTML='<<button class="w3-button w3-teal" onclick="cancel(this)">Cancel</button>'
-    console.log("ok1")
+   
 }
 
+//TO DELETE A GIVEN ROW
 function del(obj){
-    let parent=obj.parentNode
-    parent=parent.parentNode
-    // let parent=t.parent()
-    // parent=parent.parent()
-    // console.log(parent.id)
-    console.log(parent)
-    parent.parentNode.removeChild(parent)
+    let currentTableRow=obj.parentNode.parentNode
+    currentTableRow.parentNode.removeChild(currentTableRow)
 }
 
-
+//FUNCTION THAT REFRESHS DATA
 function refreshFun(){
-    event()
+    loadDataEvent()
 };
+
+//FUNCTION TO ADD A ROW 
+function addData(){
+    // myTable=document.getElementById("mytable")
+    // let newRow=myTable.insertRow()
+    // for(let i=0;i<7;i++){
+    //     let tableCell=document.createElement("th");
+        
+    //     newRow.appendChild(tableCell);
+        
+    // }
+    // let tableCell=newRow.insertCell()
+    // tableCell.innerHTML='<button class="w3-button w3-teal" onclick="edit()">Edit</button>'
+    // tableCell= newRow.insertCell();
+    // tableCell.innerhTML='<button class="w3-button w3-teal" onclick="del(this)">Delete</button>'
+    // edit()
+}
