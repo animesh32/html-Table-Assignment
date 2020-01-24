44ed5b24c0b13f58058c48c084722090137afcf1
animesh <animesh.garg@sourcefuse.com>
2020-01-24 18:42:38 +0530

    add functionality added

--- load_data_json.js
+++ load_data_json.js
@@ -37,6 +37,7 @@ let usersData=[
 //EVENT LISTENER THAT GETS TRIGGERED ON LOAD DATA BUTTON
 document.getElementById("loadbutton").addEventListener("click",loadDataEvent)
 
+
 //EVENT THAT OCCURS ON CLICKING LOAD DATA BUTTON
 function loadDataEvent(){  
     //ALL  ELEMENTS IN BODY ARE REMOVED SO TO DISPLAY NEXT PAGE
@@ -47,7 +48,7 @@ function loadDataEvent(){
     divElement.align="center"
     divElement.style.marginTop= "10px"
     divElement.style.marginBottom= "10px"
-    divElement.innerHTML='<button id="refresh" onclick="refreshFun()">Refresh Data</button>';
+    divElement.innerHTML='<button class="button1" id="refresh" onclick="refreshFun()">Refresh Data</button>';
     body.appendChild(divElement)
     
     //CREATING NEW TABLE ELEMENT
@@ -61,21 +62,27 @@ function loadDataEvent(){
     divElement.align="center"
     divElement.style.marginTop= "10px"
     divElement.style.marginBottom= "10px"
-    divElement.innerHTML='<button id="add" onclick="addData()">Add Data</button>';
+    divElement.innerHTML='<button class="button1" id="add" onclick="addData()">Add Data</button>';
     body.appendChild(divElement)
 }
 
 //CREATES TABLE ELEMENT WITH HEADERS FROM tableHeaders ARRAY
 function createTable(){
+    
+    //CREATING DIV ELEMENT
     let divElement=document.createElement("div")
     divElement.id="table-div"
     divElement.className="w3-container"
+
+    //CREATING TABLE ELEMENT
     document.getElementsByTagName("body")[0].appendChild(divElement);
     let myTable=document.createElement("table");
     myTable.id="mytable"
     myTable.className="w3-table-all w3-centered"
     document.getElementById("table-div").appendChild(myTable);
     let tableRow=myTable.insertRow();
+
+    //CREATING HEADERS OF TABLE
     for(let i=0;i<tableHeaders.length;i++){
         let tableCell=document.createElement("th");
         tableRow.appendChild(tableCell);
@@ -105,6 +112,8 @@ function fillTable(){
     }
     
 }
+
+
 // TO EDIT A GIVEN ROW
 function edit(){
     let currentTableRow=this.parentNode.parentNode
@@ -117,6 +126,7 @@ function edit(){
         currentColumn=currentColumn.nextSibling
     }
 
+    //MAKING ROWS EDITABLE
     noOfCells=7
     currentColumn=currentTableRow.firstChild
     while(noOfCells--){
@@ -130,10 +140,13 @@ function edit(){
     this.onclick=function(){//ACTION ON CLICKING SAVE BUTTON
         noOfCells=7
         currentColumn=currentTableRow.firstChild
+
+        //NOW ALLOWING ROWS TO EDIT NOW
         while(noOfCells--){
         currentColumn.contentEditable=false
         currentColumn=currentColumn.nextSibling
         }
+
         //CANGING BUTTONS BACK TO EDIT AND DELETE
         this.textContent="Edit"
         this.onclick=edit
@@ -142,16 +155,22 @@ function edit(){
     }
     let editButtonObj=this;
     delButtonCell.firstChild.textContent="Cancel"
-    delButtonCell.firstChild.onclick=function(){
+    delButtonCell.firstChild.onclick=function(){//ACTION ON CLICKING CANCEL BUTTON
+
+        //NOT ALLOWING ROWS TO EDIT NOW
         noOfCells=7
         currentColumn=currentTableRow.firstChild
         while(noOfCells--){
         currentColumn.contentEditable=false
         currentColumn=currentColumn.nextSibling
         }
+
+        //MAKING BUTTONS BACK TO EDIT AND DELETE
         editButtonObj.textContent="Edit"
         editButtonObj.onclick=edit
         currentColumn=currentTableRow.firstChild
+
+        //REPLACING NEW DATA WITH OLD DATA 
         for(let i=0;i<7;i++){
             currentColumn.textContent=rowData[i];
             currentColumn=currentColumn.nextSibling
@@ -163,30 +182,65 @@ function edit(){
    
 }
 
+
 //TO DELETE A GIVEN ROW
 function del(obj){
     let currentTableRow=obj.parentNode.parentNode
     currentTableRow.parentNode.removeChild(currentTableRow)
 }
 
+
 //FUNCTION THAT REFRESHS DATA
 function refreshFun(){
     loadDataEvent()
 };
 
+
 //FUNCTION TO ADD A ROW 
 function addData(){
-    // myTable=document.getElementById("mytable")
-    // let newRow=myTable.insertRow()
-    // for(let i=0;i<7;i++){
-    //     let tableCell=document.createElement("th");
-        
-    //     newRow.appendChild(tableCell);
-        
-    // }
-    // let tableCell=newRow.insertCell()
-    // tableCell.innerHTML='<button class="w3-button w3-teal" onclick="edit()">Edit</button>'
-    // tableCell= newRow.insertCell();
-    // tableCell.innerhTML='<button class="w3-button w3-teal" onclick="del(this)">Delete</button>'
-    // edit()
+    myTable=document.getElementById("mytable")
+
+    //CREATING A ROW AND CELLS
+    let newRow=myTable.insertRow()
+    let newCell=newRow.insertCell()
+        newCell.appendChild(document.createTextNode("enter data"))
+    for(let i=0;i<6;i++){
+        newCell=newRow.insertCell()
+        newCell.appendChild(document.createTextNode(""))
+    }
+
+    //MAKING CELLS EDITABLE
+    let noOfCells=7
+    let currentColumn=newRow.firstChild
+    while(noOfCells--){
+        currentColumn.contentEditable=true
+        currentColumn=currentColumn.nextSibling
+    }
+
+    //ADDING BUTTONS ADD AND CANCEL
+    newCell=newRow.insertCell()
+    newCell.innerHTML='<button class="w3-button w3-teal">Add</button>'
+    newCell.firstChild.onclick=function(){ //FUNCTION ON CICKING ADD BUTTON
+
+        //MAKING ROWS NON EDITABLE
+        noOfCells=7
+        currentColumn=newRow.firstChild
+        while(noOfCells--){
+        currentColumn.contentEditable=false
+        currentColumn=currentColumn.nextSibling
+
+        }
+        //CANGING BUTTONS BACK TO EDIT AND DELETE
+        this.textContent="Edit"
+        this.onclick=edit
+        this.parentNode.nextSibling.innerHTML='<button class="w3-button w3-teal" onclick="del(this)">Delete</button>'
+
+    }
+
+    // MAKING CANCEL BUTTON
+    newCell=newRow.insertCell()
+    newCell.innerHTML='<button class="w3-button w3-teal">Cancel</button>'
+    newCell.firstChild.onclick=function(){
+        del(this)//DELETING ROW ON CLICKING CANCEL BUTTON
+    }
 }
